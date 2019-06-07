5e1c03f07b949cd87b52de3f5093bbe157041fb6
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-06-06 17:39:23 -0700

    API Keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// const PASSWORD = process.env.REACT_APP_MONGO_URI_PASSWORD;
+// const SECRET = process.env.REACT_APP_SECRET;
+
+module.exports = {
+  mongoURI: `mongodb+srv://the_scoop:VictorJoseYPR@clusterfcc-yjtmu.mongodb.net/full-stack-app?`,
+  secretOrKey: `secret`
+};
