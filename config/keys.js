335e2836f8f6f64d049c29371eb9c9b2bf11d2c3
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-08 17:29:34 -0700

    Using different database, updated mongo URI

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@
 // const SECRET = process.env.REACT_APP_SECRET;
 
 module.exports = {
-  mongoURI: `mongodb+srv://the_scoop:VictorJoseYPR@clusterfcc-yjtmu.mongodb.net/full-stack-app?`,
+  mongoURI: `mongodb+srv://dbAdmin:NB4PNSMhAp3TvXbe@v1-rotate-8yvto.gcp.mongodb.net/full-stack-app?retryWrites=true&w=majority`,
   secretOrKey: `secret`
 };
