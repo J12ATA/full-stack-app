571557c878f60b0fee88ede17cda4963189b0b6b
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-15 17:25:50 -0700

    API now only focuses on making calls

---
+++ client/src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const BASE_URL = 'http://localhost:5000/api';
+
+export const getAllUserData = async () => {
+  const response = await axios.get(`${BASE_URL}/users/get_all_users`);
+  return response;
+};
+
+export const createNewUser = user => {
+  return axios.post(`${BASE_URL}/users/add_user`, {
+    name: user.name,
+    email: user.email,
+    password: user.password,
+    password2: user.password2,
+  })
+};
+
+export const deleteUser = id => {
+  return axios.delete(`${BASE_URL}/users/delete_user`, {
+    id,
+  });
+};
