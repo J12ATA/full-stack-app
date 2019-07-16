00f3dfcaa2af49e1c8fe959d6a83c0c955ef5ce5
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-15 17:21:28 -0700

    refactored and removed comments

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,130 +2,74 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_ADMIN, ADMIN_LOADING, REVIEWER_LOADING, SET_CURRENT_REVIEWER } from "./types";
+import { GET_ERRORS, SET_CURRENT_ADMIN, ADMIN_LOADING, USER_LOADING, SET_CURRENT_USER } from "./types";
 
-// Register Admin
 export const addAdmin = (userData, history) => dispatch => {
-  axios
-    .post("/api/add_admin", userData)
-    .then(res => history.push("/login_admin")) // re-direct to login on successful admin registration
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+  axios.post("/api/admin/add_admin", userData).then(res => 
+    history.push("/login_admin")
+  ).catch(err => 
+    dispatch({ type: GET_ERRORS, payload: err.response.data })
+  );
 };
 
-// AddReviewer
-export const addReviewer = (userData, history) => dispatch => {
-  axios
-    .post("/api/add_reviewer", userData)
-    .then(res => history.push("/login_reviewer")) //re-direct to users on successful reviewer registration
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const addUser = (userData, history) => dispatch => {
+  axios.post("/api/users/add_user", userData).then(res => 
+    history.push("/login_user")
+  ).catch(err =>
+    dispatch({ type: GET_ERRORS, payload: err.response.data })
+  );
 };
 
-// Login - get admin token
 export const loginAdmin = userData => dispatch => {
-  axios
-    .post("/api/login_admin", userData)
-    .then(res => {
-      // Save to localStorage
-
-      // Set token to localStorage
-      const { token } = res.data;
-      localStorage.setItem("jwtToken", token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get admin data
-      const decoded = jwt_decode(token);
-      // Set current admin
-      dispatch(setCurrentAdmin(decoded));
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+  axios.post("/api/admin/login_admin", userData).then(res => {
+    const { token } = res.data;
+    localStorage.setItem("jwtToken", token);
+    localStorage.setItem("tokenOwner", "Admin");
+    setAuthToken(token);
+    const decoded = jwt_decode(token);
+    dispatch(setCurrentAdmin(decoded));
+  }).catch(err =>
+    dispatch({ type: GET_ERRORS, payload: err.response.data })
+  );
 };
 
-// LoginReviewer - get reviewer token
-export const loginReviewer = userData => dispatch => {
-  axios
-    .post("/api/login_reviewer", userData)
-    .then(res => {
-      // Save to localStorage
-
-      // Set token to localStorage
-      const { token } = res.data;
-      localStorage.setItem("jwtToken", token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get reviewer data
-      const decoded = jwt_decode(token);
-      // Set current reviewer
-      dispatch(setCurrentReviewer(decoded));
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const loginUser = userData => dispatch => {
+  axios.post("/api/users/login_user", userData).then(res => {
+    const { token } = res.data;
+    localStorage.setItem("jwtToken", token);
+    localStorage.setItem("tokenOwner", "User");
+    setAuthToken(token);
+    const decoded = jwt_decode(token);
+    dispatch(setCurrentUser(decoded));
+  }).catch(err =>
+    dispatch({ type: GET_ERRORS, payload: err.response.data })
+  );
 };
 
-// Set logged in admin
-export const setCurrentAdmin = decoded => {
-  return {
-    type: SET_CURRENT_ADMIN,
-    payload: decoded
-  };
-};
+export const setCurrentAdmin = decoded => ({ 
+  type: SET_CURRENT_ADMIN, payload: decoded 
+});
 
-// Set logged in reviewer
-export const setCurrentReviewer = decoded => {
-  return {
-    type: SET_CURRENT_REVIEWER,
-    payload: decoded
-  };
-};
+export const setCurrentUser = decoded => ({
+  type: SET_CURRENT_USER, payload: decoded
+});
 
-// Admin loading
-export const setAdminLoading = () => {
-  return {
-    type: ADMIN_LOADING
-  };
-};
+export const setAdminLoading = () => ({
+  type: ADMIN_LOADING
+});
 
-// Reviewer loading
-export const setReviewerLoading = () => {
-  return {
-    type: REVIEWER_LOADING
-  };
-};
+export const setUserLoading = () => ({
+  type: USER_LOADING
+});
 
-// Log admin out
 export const logoutAdmin = () => dispatch => {
-  // Remove token from local storage
   localStorage.removeItem("jwtToken");
-  // Remove auth header for future requests
   setAuthToken(false);
-  // Set current admin to empty object {} which will set isAuthenticated to false
   dispatch(setCurrentAdmin({}));
 };
 
-// Log reviewer out
-export const logoutReviewer = () => dispatch => {
-  // Remove token from local storage
+export const logoutUser = () => dispatch => {
   localStorage.removeItem("jwtToken");
-  // Remove auth header for future requests
   setAuthToken(false);
-  // Set current reviewer to empty object {} which will set isAuthenticated to false
-  dispatch(setCurrentReviewer({}));
+  dispatch(setCurrentUser({}));
 }
