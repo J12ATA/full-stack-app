6ca66575d576fa912ed2a03b5f2a5dc4fe72b820
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-15 17:15:14 -0700

    added support for cors and improved error handler

--- server.js
+++ server.js
@@ -6,43 +6,40 @@ const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const reviewRoutes = require("./routes/review");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 
-// use the MongoDB drivers
 mongoose.set("useFindAndModify", false);
 
-// Bodyparser middleware
+app.use(cors());
+app.options('*', cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
 const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
 mongoose.connect(db, { useNewUrlParser: true }).then(() => {
   if (mongoose.connection.readyState !== 1) console.log("MongoDB disconnected")
 }).catch(err => next(err));
 
-// Passport middleware
 app.use(passport.initialize());
 
-// Passport config
+
 require("./config/passport")(passport);
 
-// Routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-// Error Handling middleware
 app.use((err, req, res, next) => {
-  const { status, message } = err;
-  res.status(status || 500).type("txt").send(message || "Internal Server Error");
+  const { errors, status } = err;
+  res.status(status || 500).send(errors || "Internal Server Error");
 });
 
-if (!module.parent) app.listen(port, () => 
+if (!module.parent) app.listen(port, () =>
   console.log(`Server up and running on port ${port}!`)
 );
 
