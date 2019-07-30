4e98b40d4d4920e8280488a2f99dd3a27fb9f64d
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-29 19:06:18 -0700

    removed unused action types, & tokenOwner

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 
-import { GET_ERRORS, SET_CURRENT_ADMIN, ADMIN_LOADING, USER_LOADING, SET_CURRENT_USER } from "./types";
+import { GET_ERRORS, SET_CURRENT_ADMIN, SET_CURRENT_USER } from "./types";
 
 const BASE_URL = "http://35.232.153.179/api";
 
@@ -56,22 +56,16 @@ export const setCurrentUser = decoded => ({
   type: SET_CURRENT_USER, payload: decoded
 });
 
-export const setAdminLoading = () => ({
-  type: ADMIN_LOADING
-});
-
-export const setUserLoading = () => ({
-  type: USER_LOADING
-});
-
 export const logoutAdmin = () => dispatch => {
   localStorage.removeItem("jwtToken");
+  localStorage.removeItem("tokenOwner");
   setAuthToken(false);
   dispatch(setCurrentAdmin({}));
 };
 
 export const logoutUser = () => dispatch => {
   localStorage.removeItem("jwtToken");
+  localStorage.removeItem("tokenOwner");
   setAuthToken(false);
   dispatch(setCurrentUser({}));
 }
