d5472d614deda2ceeb5c3eb2db537da131559e57
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-30 02:48:26 -0700

    enabled access to react-router via withRouter HOC

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 class Navbar extends Component {
   render() {
@@ -11,4 +12,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(Navbar);
+export default withRouter(connect(mapStateToProps)(Navbar));
