77a290c78c06e743ee7489d7d5b011ebd19cea81
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-30 02:50:45 -0700

    added PropTypes to Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import PropTypes from "prop-types";
 
 class Navbar extends Component {
   render() {
     return <div>{/* home of new navbar */}</div>;
   }
 }
+
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
