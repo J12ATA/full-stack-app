292abe118f73ea30d4354269ef35c2e24050239b
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-29 19:07:24 -0700

    removed unused action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,5 @@
 export const GET_ERRORS = "GET_ERRORS";
-export const ADMIN_LOADING = "ADMIN_LOADING";
 export const SET_CURRENT_ADMIN = "SET_CURRENT_ADMIN";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
-export const USER_LOADING = "USER_LOADING";
 export const SET_USER_DATA = "SET_USER_DATA";
 export const SET_PRODUCT_DATA = "SET_PRODUCT_DATA";
