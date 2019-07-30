de10ccffeee98b868f8dbd8660075e4050a08728
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-30 02:34:40 -0700

    navbar will be rewritten from scratch using mdc

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   render() {
-    return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace",
-                width: "100vw"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              FULL STACK APP
-            </Link>
-          </div>
-        </nav>
-      </div>
-    );
+    return <div>{/* home of new navbar */}</div>;
   }
 }
 export default Navbar;
