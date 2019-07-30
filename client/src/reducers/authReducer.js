eeb3e47427d7f71db0341d08e469538c5d32b528
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-29 19:32:43 -0700

    removed unused loading actions from authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,12 +1,11 @@
-import { SET_CURRENT_ADMIN, SET_CURRENT_USER, ADMIN_LOADING, USER_LOADING } from "../actions/types";
+import { SET_CURRENT_ADMIN, SET_CURRENT_USER } from "../actions/types";
 
 const isEmpty = require("is-empty");
 
 const initialState = {
   isAuthenticated: false,
   admin: {},
-  user: {},
-  loading: false
+  user: {}
 };
 
 export default function(state = initialState, action) {
@@ -17,21 +16,11 @@ export default function(state = initialState, action) {
         isAuthenticated: !isEmpty(action.payload),
         admin: action.payload
       };
-    case ADMIN_LOADING:
-      return {
-        ...state,
-        loading: true
-      };
     case SET_CURRENT_USER:
       return {
         ...state,
         isAuthenticated: !isEmpty(action.payload),
         user: action.payload
-      };
-    case USER_LOADING:
-      return {
-        ...state,
-        loading: true
       };
     default:
       return state;
