7c4eaf0ee51632a0eebc9f720215eb8ee712d202
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-29 19:30:46 -0700

    error is now handled as per documentation

--- config/passport.js
+++ config/passport.js
@@ -9,12 +9,10 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      Admin.findById(jwt_payload._id).then(user => {
-        if (user) return done(null, user);
-        return done(null, false);
-      }).catch(err => console.log(err));
-    })
-  );
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    Admin.findById(jwt_payload._id).then(user => {
+      if (user) return done(null, user);
+      return done(null, false);
+    }).catch(err => done(err, false));
+  }));
 };
