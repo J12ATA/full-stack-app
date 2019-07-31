d86343478ae050c160e33e81d1445a2a20defc44
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-07-30 21:28:04 -0700

    took auth check out of global scope

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,12 @@ import "./app.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentAdmin, setCurrentUser, logoutUser, logoutAdmin } from "./actions/authActions";
+import {
+  setCurrentAdmin,
+  setCurrentUser,
+  logoutUser,
+  logoutAdmin
+} from "./actions/authActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -19,31 +24,27 @@ import PrivateRoute from "./components/private-route/PrivateRoute";
 import AdminDashboard from "./components/dashboard/AdminDashboard";
 import UserDashboard from "./components/dashboard/UserDashboard";
 
-if (localStorage.jwtToken && localStorage.tokenOwner === "User") {
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  const decoded = jwt_decode(token);
-  store.dispatch(setCurrentUser(decoded));
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-    window.location.href = "./login_user";
-  }
-}
-
-if (localStorage.jwtToken && localStorage.tokenOwner === "Admin") {
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  const decoded = jwt_decode(token);
-  store.dispatch(setCurrentAdmin(decoded));
-  const currentTime = Date.now() / 1000; 
-  if (decoded.exp < currentTime) {
-    store.dispatch(logoutAdmin());
-    window.location.href = "./login_admin";
-  }
-}
-
 class App extends Component {
+  componentDidMount() {
+    const { jwtToken, tokenOwner } = localStorage;
+
+    if (jwtToken) {
+      const decoded = jwt_decode(jwtToken);
+      const currentTime = Date.now() / 1000;
+
+      setAuthToken(jwtToken);
+
+      tokenOwner === "Admin"
+        ? store.dispatch(setCurrentAdmin(decoded))
+        : store.dispatch(setCurrentUser(decoded));
+
+      if (decoded.exp < currentTime) {
+        store.dispatch(logoutAdmin());
+        store.dispatch(logoutUser());
+      }
+    }
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -56,8 +57,16 @@ class App extends Component {
             <Route exact path="/login_admin" component={LoginAdmin} />
             <Route exact path="/login_user" component={LoginUser} />
             <Switch>
-              <PrivateRoute exact path="/admin_dashboard" component={AdminDashboard} />
-              <PrivateRoute exact path="/user_dashboard" component={UserDashboard} />
+              <PrivateRoute
+                exact
+                path="/admin_dashboard"
+                component={AdminDashboard}
+              />
+              <PrivateRoute
+                exact
+                path="/user_dashboard"
+                component={UserDashboard}
+              />
             </Switch>
           </div>
         </Router>
