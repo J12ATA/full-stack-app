e638756f9aceb397683678b6017f52935190ff5d
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-02 00:17:01 -0700

    added types for login data and dialog

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const GET_ERRORS = "GET_ERRORS";
+export const GET_LOGIN_DATA = "GET_LOGIN_DATA";
 export const SET_CURRENT_ADMIN = "SET_CURRENT_ADMIN";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const SET_USER_DATA = "SET_USER_DATA";
 export const SET_PRODUCT_DATA = "SET_PRODUCT_DATA";
+export const TOGGLE_LOGIN = "TOGGLE_LOGIN";
