a7e9d4ad9a237a723210f2ba40353328450cfcfa
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-02 16:57:44 -0700

    removed button element bellow table component

--- client/src/components/dashboard/loadingDashboard.js
+++ client/src/components/dashboard/loadingDashboard.js
@@ -29,18 +29,6 @@ class loadingDashboard extends Component {
                 emptyRowsWhenPaging: false
               }}
             />
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
           </div>
         </div>
       </div>
