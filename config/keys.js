029e977eee07e89d8d271f815949dedd939abce2
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-17 19:53:21 -0700

    access env vars for security best practice

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-// const PASSWORD = process.env.REACT_APP_MONGO_URI_PASSWORD;
-// const SECRET = process.env.REACT_APP_SECRET;
+const { CLUSTER, DATABASE, PASSWORD, SECRET, USER } = process.env;
 
 module.exports = {
-  mongoURI: `mongodb+srv://dbAdmin:NB4PNSMhAp3TvXbe@v1-rotate-8yvto.gcp.mongodb.net/full-stack-app?retryWrites=true&w=majority`,
-  secretOrKey: `secret`
+  mongoURI: `mongodb+srv://${USER}:${PASSWORD}@${CLUSTER}-8yvto.gcp.mongodb.net/${DATABASE}?retryWrites=true&w=majority`,
+  secretOrKey: `${SECRET}`
 };
