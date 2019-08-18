57a30c90ed1538aa1719fa6b900bc4e2a8fc0327
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-17 19:59:49 -0700

    corrected promise error handling

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
     if (mongoose.connection.readyState !== 1)
       console.log("MongoDB disconnected");
   })
-  .catch(err => next(err));
+  .catch(err => console.log(err));
 
 app.use(passport.initialize());
 
