1894974fd7732c571d3ad4c001055458af08f889
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-18 18:59:59 -0700

    we will be using the action creator directly

--- client/src/actions/titleActions.js
+++ client/src/actions/titleActions.js
@@ -1,9 +1,5 @@
 import { SET_NAV_TITLE } FROM "./types";
 
-export const updateNavTitle = location => dispatch => {
-  dispatch(setNavTitle(location));
-};
-
 export const setNavTitle = location => ({
   type: SET_NAV_TITLE,
   title: location
