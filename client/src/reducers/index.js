5993d2ad74dcc448dc03029f20f002041eaeb372
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-08-18 18:46:33 -0700

    added titleReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import userReducer from "./userReducer";
+import titleReducer from "./titleReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  users: userReducer
+  users: userReducer,
+  title: titleReducer
 });
