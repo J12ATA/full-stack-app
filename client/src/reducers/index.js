24cf7ba72369f1e15273b2da22077df32572295b
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-10-04 18:53:43 -0700

    addded navItem & navReducer to index file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import userReducer from "./userReducer";
 import titleReducer from "./titleReducer";
+import navReducer from "./navReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   users: userReducer,
-  title: titleReducer
+  title: titleReducer,
+  navItem: navReducer
 });
